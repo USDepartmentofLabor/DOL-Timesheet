4d7bab068c39320d5522e50c57ad5a4c0d60d6b3
ggruse <gruse.george.g@dol.gov>
2024-04-24 09:56:51 -0400

    Fixed QA Issues

--- DOL-Timesheet/ViewController/Timesheet/TimesheetSoftenViewController.swift
+++ DOL-Timesheet/ViewController/Timesheet/TimesheetSoftenViewController.swift
@@ -56,6 +56,7 @@ class TimesheetSoftenViewController: UIViewController, TimeViewDelegate, TimePic
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setupEmploymentPopupButton()
+        setupView()
         displayInfo()
     }
     
